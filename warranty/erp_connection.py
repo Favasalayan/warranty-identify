199a8e821e8449bfb7d16492140087ace652a7a5
Muhammed Favas <YOUR_GITHUB_EMAIL@example.com>
2026-03-16 16:33:08 +0300

    Updated ERP connection and settings

--- warranty/erp_connection.py
+++ warranty/erp_connection.py
@@ -8,10 +8,10 @@ def get_erp_connection():
     return pyodbc.connect(
         
         "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=192.168.105.254;"
-        "DATABASE=Kakiya_April_18_2023;"
-        "UID=Pricecheck;"
-        "PWD=hc@123456;"
+        "SERVER=example;"
+        "DATABASE=example;"
+        "UID=example;"
+        "PWD=example;"
         "TrustServerCertificate=yes;"
         
     )
--- warranty_identify/settings.py
+++ warranty_identify/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-7se2yfwtgfbva6xg%$q#!-t7tprz&3e1b(=8bt-j-)ngoggwgj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,3 +121,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
